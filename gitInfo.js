5795ce1fb6ed17c7672e82c60028f247127f3e15
Benjamin Salmon <bensalmon8@gmail.com>
2022-05-06 13:13:05 -0400

    Assessment Complete

---
+++ gitInfo.js
@@ -0,0 +1,92 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+
+let gitDefinition = ('Git is a program, not to be confused with GitHub which is a website. Git is a version control system that allows individuals and teams to track changes to a code or project. It prevents bugs and loss of progress and allows you to view the long history of a project in snapshorts.')
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+lt gitHubDefinition = ('Git hub is a website that allows you to take your local repositories and upload them to the cloud service or even download them.')
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+
+let gitInitDefinition = ('Git init means initialization, or we are starting up. Tells us the folder that we are currently sitting in is a project and we want to track everything inside the folder.')
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+
+let gitCloneDefinition = ('Git clone allows you to take a copy of a repository from Github and put it on your computer.')
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+
+let gitStatusDefinition = ('Git status tells about the repository.')
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+
+let gitAddDefinition = ('Git add allows files to be tracked.')
+let gitAddCode = ('git add')
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+let gitCommitDefinition = ('Git commit is when we commit to store is when we store all the information.  It creates a snapshot of the files being tracked.')
+let gitCommitCode = ('git commit -m "initial commmit" ')
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+
+let gitPushDefinition = ('Git push allows us to save the informationa and send the information of to Github to be stored somewhere else.')
